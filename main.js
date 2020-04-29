3ef8bbfa42142e023acf10e86d1afc0c6c95587b
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-29 14:47:59 +0200

    esercizio

---
+++ main.js
@@ -0,0 +1,10 @@
+//facendo click sull'icona menu (bars) il menu diventa visibile
+$('.header-right > a').click(function() {
+    //il menu diventa visibile
+    $('.hamburger-menu').show();
+})
+//facendo click sull'icona X il menu scompare
+$('.fa-times').click(function(){
+
+    $('.hamburger-menu').hide();
+})
